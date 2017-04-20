98d5b2d862e6adf19a67cc93394f635804e2d34d
Ryan Manwiller <rydama@gmail.com>
2017-04-20 15:44:53 -0700

    Fix for Rails 5

--- lib/carmen/rails/action_view/form_helper.rb
+++ lib/carmen/rails/action_view/form_helper.rb
@@ -220,6 +220,36 @@ module ActionView
           end
         end
       end
+    else
+      # Rails 5 support
+      module Tags
+        class Base
+          def to_region_select_tag(parent_region, options = {}, html_options = {})
+            html_options = html_options.stringify_keys
+            add_default_name_and_id(html_options)
+
+            # select_not_required? was replaced in Rails 5 with placeholder_required?
+            # options[:include_blank] ||= true unless options[:prompt] || select_not_required?(html_options)
+            if placeholder_required?(html_options)
+              raise ArgumentError, "include_blank cannot be false for a required field." if options[:include_blank] == false
+              options[:include_blank] ||= true unless options[:prompt]
+            end
+
+            value = options[:selected] ? options[:selected] : value(object)
+            priority_regions = options[:priority] || []
+            opts = add_options(region_options_for_select(parent_region.subregions, value,
+                                                        :priority => priority_regions),
+                               options, value)
+            select = content_tag("select", opts, html_options)
+            if html_options["multiple"] && options.fetch(:include_hidden, true)
+              tag("input", :disabled => html_options["disabled"], :name => html_options["name"],
+                           :type => "hidden", :value => "") + select
+            else
+              select
+            end
+          end
+        end
+      end
     end
 
     class FormBuilder
